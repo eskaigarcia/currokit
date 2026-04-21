baff7c60b7b2cac5ca78173e15cb4a975e5ca422
Eskai Garcia <yosoyeskai@gmail.com>
2026-04-21 20:05:19 +0200

    admin: security template clean up

--- resources/js/types/auth.ts
+++ resources/js/types/auth.ts
@@ -3,8 +3,6 @@ export type User = {
     name: string;
     email: string;
     avatar?: string;
-    email_verified_at: string | null;
-    two_factor_enabled?: boolean;
     created_at: string;
     updated_at: string;
     [key: string]: unknown;
@@ -12,13 +10,4 @@ export type User = {
 
 export type Auth = {
     user: User;
-};
-
-export type TwoFactorSetupData = {
-    svg: string;
-    url: string;
-};
-
-export type TwoFactorSecretKey = {
-    secretKey: string;
 };
